e414ec50194ec702925b5c85e6e18df2b4960afd
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-05-11 10:38:03 +0430

    updated it name of database

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,8 +3,8 @@ const mysql=require('mysql2/promise')
 const db=mysql.createPool({
     host:'localhost',
     user:'root',
-    password:'12345',
+    password:'1234',
     port:3306,
-    database:'ourdb'
+    database:'userd'
 })
 module.exports=db
